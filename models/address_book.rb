7d308e61d0608d83d285098ea7c8594a898b4e70
jagsarai <sarai.jagvir@gmail.com>
2017-06-09 22:19:02 -0700

    added csv files

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative 'entry'
+require "csv"
 
 class AddressBook
   attr_reader :entries
@@ -9,23 +10,33 @@ class AddressBook
 
   def add_entry(name, phone_number, email)
     index = 0
-    @entries.each do |entry|
+    entries.each do |entry|
       if name < entry.name
         break
       end
       index+= 1
     end
-    @entries.insert(index, Entry.new(name, phone_number, email))
+    entries.insert(index, Entry.new(name, phone_number, email))
   end
 
-  def remove_entry(name, phone_number, email)
-    delte_entry = nil
+  # def remove_entry(name, phone_number, email)
+  #   delte_entry = nil
+  #
+  #   @entries.each do |entry|
+  #     if name == entry.name && phone_number == entry.phone_number && email == entry.email
+  #       delte_entry = entry
+  #     end
+  #   end
+  #   @entries.delete(delte_entry)
+  # end
 
-    @entries.each do |entry|
-      if name == entry.name && phone_number == entry.phone_number && email == entry.email
-        delte_entry = entry
-      end
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
-    @entries.delete(delte_entry)
   end
 end
